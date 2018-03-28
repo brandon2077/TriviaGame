2ec4fcede6bd2c332f2114bfe888a04723e245b7
brandonao <brandonowensgd@gmail.com>
2018-03-28 13:20:26 -0700

    add index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,224 +1,209 @@
-// Define variables
+// Defining the basic variables
 $(document).ready(function(){
+    var counter;
     var count = 0;
-    var time = 31;
+    var timer = 31;
+    var passed = 0;
+    var missed = 0;
+    var silence = 0;
     var isSelected = false;
-    var ticker;
-    var correct = 0;
-    var incorrect = 0;
-    var unanswered = 0;
 
-// Questions and Answer Arrays
-    var question = ["How many Tenets did the Dark Brotherhood originally follow?","What race was the Night Mother in life?","Which of the following Daedra is once said to have appeared as the Night Mother, however this was later deemed incorrect?","What is the Blade of Woe?","What Dark Brotherhood rank does the jester Cicero retain at the time when he arrives in the Falkreath Sanctuary in Skyrim?","What race are the Shadowscales?","In which city in Cyrodiil can the Hero of Kvatch enter a Sanctuary?"];
-    var answer = ["Five","Dunmer","Mephala","A Daedric Artefact","Keeper","Argonian","Cheydinhal","Shadowmere"];
-    var firstChoice = ["Five","Dunmer","Molag Bal","A sword","Murderer","Kajiit","Bruma","Darkracer"];
-    var secondChoice = ["Three","Nord","Meridia","A Daedric Artefact","Silencer","Dov","Bravil","Frost"];
-    var thirdChoice = ["Ten","Altmer","Mephala","A poem","Keeper","Hist","Kvatch","Shadowmere"];
-    var fourthChoice = ["Fifty-two","Daedra","Azura","A dagger","Listener","Argonian","Cheydinhal","Evilmane"];
+// Placing the questionss and answerss into arrays
+// Then placing the arrays into variables
+    var questions = ["How many Tenets did the Dark Brotherhood originally follow?","What race was the Night Mother in life?","Which of the following Daedra is once said to have appeared as the Night Mother, however this was later deemed incorrect?","What is the Blade of Woe?","What Dark Brotherhood rank does the jester Cicero retain at the time when he arrives in the Falkreath Sanctuary in Skyrim?","What race are the Shadowscales?","In which city in Cyrodiil can the Hero of Kvatch enter a Sanctuary?"];
+    var answers = ["Five","Dunmer","Mephala","A Daedric Artefact","Keeper","Argonian","Cheydinhal","Shadowmere"];
+    var choiceOne = ["Five","Dunmer","Molag Bal","A sword","Murderer","Kajiit","Bruma","Darkracer"];
+    var choiceTwo = ["Three","Nord","Meridia","A Daedric Artefact","Silencer","Dov","Bravil","Frost"];
+    var choiceThree = ["Ten","Altmer","Mephala","A poem","Keeper","Hist","Kvatch","Shadowmere"];
+    var choiceFour = ["Fifty-two","Daedra","Azura","A dagger","Listener","Argonian","Cheydinhal","Evilmane"];
 
-// Show & Hide Functions
-    function showHolders() {
-        $("#question-holder").show();
-        $("#choice-holder-1").show();
-        $("#choice-holder-2").show();
-        $("#choice-holder-3").show();
-        $("#choice-holder-4").show();
+// Begin Functions
+
+// Answer Image Functions 
+    function showImages() {
+        if(count === 0) {
+            $("#imageColumn").show();
+            $("#imageColumn").html('<img src="assets/images/tenents.jpg">');
+        }
+        else if(count === 1) {
+            $("#imageColumn").show();
+            $("#imageColumn").html('<img src="assets/images/nightmother.jpg">');
+        }
+        else if(count === 2) {
+            $("#imageColumn").show();
+            $("#imageColumn").html('<img src="assets/images/mephala.jpg">');
+        }
+        else if(count === 3) {
+            $("#imageColumn").show();
+            $("#imageColumn").html('<img src="assets/images/bladeofwoe.jpg">');
+        }
+        else if(count === 4) {
+            $("#imageColumn").show();
+            $("#imageColumn").html('<img src="assets/images/cicero.png">');
+        }
+        else if(count === 5) {
+            $("#imageColumn").show();
+            $("#imageColumn").html('<img src="assets/images/shadowscale.jpg">');
+        }
+        else if(count === 6) {
+            $("#imageColumn").show();
+            $("#imageColumn").html('<img src="assets/images/sanct.png">');
+        }
+        else if(count === 7) {
+            $("#imageColumn").show();
+            $("#imageColumn").html('<img src="assets/images/shadowmere.png">');
+        }
     }
-    function hideHolders() {
-        $("#question-holder").hide();
-        $("#choice-holder-1").hide();
-        $("#choice-holder-2").hide();
-        $("#choice-holder-3").hide();
-        $("#choice-holder-4").hide();
+// Question Functions	
+    function showColumns() {
+        $("#questionsColumn").show();
+        $("#choiceColumn-1").show();
+        $("#choiceColumn-2").show();
+        $("#choiceColumn-3").show();
+        $("#choiceColumn-4").show();
     }
-    function hideResults() {
-        $("#correct-holder").hide();
-        $("#incorrect-holder").hide();
-        $("#unanswered-holder").hide();
-        $("#restart-holder").hide();
+    function hideColumns() {
+        $("#questionsColumn").hide();
+        $("#choiceColumn-1").hide();
+        $("#choiceColumn-2").hide();
+        $("#choiceColumn-3").hide();
+        $("#choiceColumn-4").hide();
+    }
+    function hideScore() {
+        $("#passedColumn").hide();
+        $("#missedColumn").hide();
+        $("#silenceColumn").hide();
+        $("#restartColumn").hide();
     }
     function displayQuestion () {
-        hideResults();
-        $("#answer-holder").hide();
-        $("#image-holder").hide();
-        $("#time-holder").show();
-        showHolders();
-        $("#question-holder").html(question[count]);
-        $("#choice-holder-1").html(firstChoice[count]);
-        $("#choice-holder-2").html(secondChoice[count]);
-        $("#choice-holder-3").html(thirdChoice[count]);
-        $("#choice-holder-4").html(fourthChoice[count]);
+        hideScore();
+        $("#answersColumn").hide();
+        $("#imageColumn").hide();
+        $("#timerColumn").show();
+        showColumns();
+        $("#questionsColumn").html(questions[count]);
+        $("#choiceColumn-1").html(choiceOne[count]);
+        $("#choiceColumn-2").html(choiceTwo[count]);
+        $("#choiceColumn-3").html(choiceThree[count]);
+        $("#choiceColumn-4").html(choiceFour[count]);
 
-    // Hover CSS
-        $("#choice-holder-1").hover(function() {
-            $(this).css("color", "gray");
+        $("#choiceColumn-1").hover(function() {
+            $(this).css("color", "#a76284");
         },
         function(){
-            $(this).css("color", "black");
+            $(this).css("color", "#743e50");
         });
-        $("#choice-holder-2").hover(function() {
-            $(this).css("color", "gray");
+        $("#choiceColumn-2").hover(function() {
+            $(this).css("color", "#a76284");
         },
         function(){
-            $(this).css("color", "black");
+            $(this).css("color", "#743e50");
         });
-        $("#choice-holder-3").hover(function() {
-            $(this).css("color", "gray");
+        $("#choiceColumn-3").hover(function() {
+            $(this).css("color", "#a76284");
         },
         function(){
-            $(this).css("color", "black");
+            $(this).css("color", "#743e50");
         });
-        $("#choice-holder-4").hover(function() {
-            $(this).css("color", "gray");
+        $("#choiceColumn-4").hover(function() {
+            $(this).css("color", "#a76284");
         },
         function(){
-            $(this).css("color", "black");
+            $(this).css("color", "#743e50");
         });
     }
-    $("#choice-holder-1").on("click", checkAnswer)
-    $("#choice-holder-2").on("click", checkAnswer)
-    $("#choice-holder-3").on("click", checkAnswer)
-    $("#choice-holder-4").on("click", checkAnswer)
-
-// Check Answer Function
+    $("#choiceColumn-1").on("click", checkAnswer)
+    $("#choiceColumn-2").on("click", checkAnswer)
+    $("#choiceColumn-3").on("click", checkAnswer)
+    $("#choiceColumn-4").on("click", checkAnswer)
+// Answer functions 
     function checkAnswer() {
-
-        hideHolders();
-
-        if($(this).text() === answer[count]) {
+        hideColumns();
+        if($(this).text() === answers[count]) {
             stopTime();
             isSelected = true;
-            $("#answer-holder").show();
-            $("#answer-holder").html("Right! The answer is: " + answer[count]);
-            displayImage();
-            correct++;
+            $("#answersColumn").show();
+            $("#answersColumn").html("Passed! The passphrase is: " + answers[count]);
+            showImages();
+            passed++;
             count++;
         }
         else {
             stopTime();
             isSelected = true;
-            $("#answer-holder").show();
-            $("#answer-holder").html("Wrong! The answer is: " + answer[count]);
-            displayImage();
-            incorrect++;
+            $("#answersColumn").show();
+            $("#answersColumn").html("Failed! The passphrase is: " + answers[count]);
+            showImages();
+            missed++;
             count++;
         }
 
         checkGameEnd();
     }
-
-// Chekc End Game Function
+// End game funcion
     function checkGameEnd() {
-        if(count === question.length) {
-            $("#time-holder").hide();
-            showResults();
+        if(count === questions.length) {
+            $("#timerColumn").hide();
+            showScore();
             count = 0;
             $(".start").show();
             $(".start").on("click", function() {
-                resetResults();
+                resetScore();
                 startGame();
             });
         }
     }
-
     function resetTime() {
-        time = 31;
+        timer = 31;
     }
-
     function displayTime() {
-        time--;
-        $("#time-holder").html("Time remaining: " + time);
-
-            if(time <= 0) {
-                hideHolders();
+        timer--;
+        $("#timerColumn").html("You have " + timer + " seconds remaining");
+            if(timer <= 0) {
+                hideColumns();
                 stopTime();
-                $("#answer-holder").show();
-                $("#answer-holder").html("Time is up! The answer is: " + answer[count]);
-                displayImage();
-                unanswered++;
+                $("#answersColumn").show();
+                $("#answersColumn").html("You've chosen silence. The passphrase is: " + answers[count]);
+                showImages();
+                silence++;
                 count++;
                 checkGameEnd();
             }
     }
-
     function startTime() {
-        clearInterval(ticker);
-        ticker = setInterval(displayTime, 1000);
+        clearInterval(counter);
+        counter = setInterval(displayTime, 1000);
     }
     function stopTime() {
-        clearInterval(ticker);
+        clearInterval(counter);
         resetTime();
-        if(count < question.length - 1) {
+        if(count < questions.length - 1) {
             setTimeout(startTime, 2000);
             setTimeout(displayQuestion, 3000);
         }
     }
-
     resetTime();
-
-// Display Images With Answer
-    function displayImage() {
-        if(count === 0) {
-            $("#image-holder").show();
-            $("#image-holder").html('<img src="assets/images/tenents.jpg">');
-        }
-        else if(count === 1) {
-            $("#image-holder").show();
-            $("#image-holder").html('<img src="assets/images/nightmother.jpg">');
-        }
-        else if(count === 2) {
-            $("#image-holder").show();
-            $("#image-holder").html('<img src="assets/images/mephala.jpg">');
-        }
-        else if(count === 3) {
-            $("#image-holder").show();
-            $("#image-holder").html('<img src="assets/images/bladeofwoe.jpg">');
-        }
-        else if(count === 4) {
-            $("#image-holder").show();
-            $("#image-holder").html('<img src="assets/images/cicero.png">');
-        }
-        else if(count === 5) {
-            $("#image-holder").show();
-            $("#image-holder").html('<img src="assets/images/shadowscale.jpg">');
-        }
-        else if(count === 6) {
-            $("#image-holder").show();
-            $("#image-holder").html('<img src="assets/images/sanct.png">');
-        }
-        else if(count === 7) {
-            $("#image-holder").show();
-            $("#image-holder").html('<img src="assets/images/shadowmere.png">');
-        }
+// Score function
+    function showScore() {
+        $("#passedColumn").show();
+        $("#passedColumn").html("Passed: " + passed);
+        $("#missedColumn").show();
+        $("#missedColumn").html("Failed: " + missed);
+        $("#silenceColumn").show();
+        $("#silenceColumn").html("Silence: " + silence);
+        $("#restartColumn").show();
+        $("#restartColumn").html("Click Enter to Retry!");
     }
-
- // Show Results Function
-    function showResults() {
-        $("#correct-holder").show();
-        $("#correct-holder").html("Correct: " + correct);
-        $("#incorrect-holder").show();
-        $("#incorrect-holder").html("Incorrect: " + incorrect);
-        $("#unanswered-holder").show();
-        $("#unanswered-holder").html("Unanswered: " + unanswered);
-        $("#restart-holder").show();
-        $("#restart-holder").html("Click Start above to play again!");
+    function resetScore() {
+        passed = 0;
+        missed = 0;
+        silence = 0;
     }
-
-// Reset Results Function
-    function resetResults() {
-        correct = 0;
-        incorrect = 0;
-        unanswered = 0;
-    }
-
-// Start Game Function
     function startGame() {
         $(".start").hide();
         startTime();
         displayQuestion();
     }
-
-// Start Game On Click
   $(".start").on("click", function() {
     startGame();
   });
